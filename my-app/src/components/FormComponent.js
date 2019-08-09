bd7cc02a98541e2963b799699ce0c4ddb6afc342
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-08-09 11:52:02 -0400

    built a registration form with formik, used yup form validation, and turned App into a class component

---
+++ my-app/src/components/FormComponent.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+
+function FormComponent({ values, errors, touched }) {
+  return (
+    <Form>
+      <div>
+        {touched.username && errors.username && <p>{errors.username}</p>}
+        <Field type="text" name="username" placeholder="Username" />
+      </div>
+      <div>
+        {touched.password && errors.password && <p>{errors.password}</p>}
+        <Field type="password" name="password" placeholder="Password" />
+      </div>
+      <button type="submit">Submit!</button>
+    </Form>
+  );
+}
+
+const FormikFormComponent = withFormik({
+  mapPropsToValues({ username, password }) {
+    return {
+      username: username || "",
+      password: password || ""
+    };
+  },
+
+  validationSchema: Yup.object().shape({
+    username: Yup.string().required("Username is required"),
+    password: Yup.string().required("Password is required")
+      .min(8, "Password must be at least 8 characters long")
+  }),
+
+  handleSubmit(values) {
+    console.log(values);
+  }
+})(FormComponent);
+
+export default FormikFormComponent;
